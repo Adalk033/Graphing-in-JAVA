1a935b8dd5489cde773401c8316306f1b989a853
Adalk033 <37385524+Adalk033@users.noreply.github.com>
2020-07-06 17:51:49 -0500

    Update PuntosEcuacion.java

--- Graficadora de ecuaciones/PuntosEcuacion.java
+++ Graficadora de ecuaciones/PuntosEcuacion.java
@@ -1,125 +1,137 @@
 /**
-* 
-* 
-* @version 3/17/2019
-*/
-
-import java.util.Vector; //Libreria para poder usar vectores
-import java.awt.geom.*;
+ * Esta clase tiene el trabajo de crear la base de las graficas con su plano cartesiano.
+ *
+ * @author Cristhian Adal Garcia Hdez.
+ * @version 2.9.5.6
+ */
+import java.util.Vector;
 import java.awt.*;
-import javax.swing.JFrame;
-public class PuntosEcuacion extends JFrame
+import java.awt.geom.*;
+import javax.swing.JOptionPane;
+
+public class PuntosEcuacion extends Frame implements Runnable
 {
-    private Vector<Punto> puntos;//vector de tipo punto
-    private Polinomio poli;
-    
-    
-    public PuntosEcuacion(Polinomio Poli,double Linf,double Lsup,double Inc)
+    private Vector<Punto> puntos;
+    private Polinomio polinomio;
+    private  Vector<Termino> termin;
+    private double li;
+    private Thread thread;
+    private int cuenta=0;
+    private Color coloritos;
+    public PuntosEcuacion(Polinomio polinomio,double limiteInferior,double lsup,double inc, Color color_2)
     {
-        super("Area de Dibujo2D");
-        
-        this.poli = Poli;
-        puntos = new Vector();
-       
-         if(Linf<Lsup){
-            for(double i=Linf;i<=Lsup;i+=Inc)
-                puntos.addElement(new Punto (i,poli.evalua(i)));
+        this.polinomio = polinomio;
+        puntos = new Vector<Punto>();
+        li = limiteInferior;       
+        for(int index=0; index <= ( ( lsup-limiteInferior ) / inc ); index++ )
+        {
+            puntos.addElement( new Punto( li, this.polinomio.evalua( li ) ) );
+            li = li + inc;
         }
-        else 
-            puntos.addElement(new Punto (Linf, poli.evalua(Linf)));
-            
-        
-        }
-        
-        
-    
-    
-     public Vector getPuntosEcuacion(){
-        return this.puntos;
+        setSize(900,900);        
+        setVisible(true);
+        thread = new Thread(this);
+        thread.start();
+        coloritos = color_2;
     }
     
-    public Punto getPunto(int num){
-        return this.puntos.elementAt(num);
+    public PuntosEcuacion()
+    {
+        super("Graficacion de Funciones");   
     }
     
-    /* private void imprimir(){
-        System.out. println("X: \t\t Y:");
-        for(int i = 0;i < puntos.size();i++){
-            System.out.println(puntos.elementAt(i).getX() 
-            + "\t\t" + puntos.elementAt(i).getY());
-        }
-    }*/
+    public Vector getPuntosEcuacion()
+    {
+        return puntos;
+    }
+    
+    public void run()
+    {
+        
+    }
     
     public boolean handleEvent(Event e)
     {
-        if(e.id == Event.WINDOW_DESTROY) //preguntar que si lp que esta pasando es un cirre de ventana (evento)
+        if(e.id == Event.WINDOW_DESTROY)//Ayuda a saber si el clic es un cierre de ventana 
            {
-               hide(); //esconderla
-               dispose(); //liberar los recursos, matarla
+               hide(); 
+               dispose(); //libera los recursos de la ventana
                return true;
             }
         return super.handleEvent(e);        
-        
     }
-   public void paint(Graphics gc)
-   {
-       //DIBUJA EL PLANO
-       Graphics2D g2d = (Graphics2D) gc;
-       g2d.setStroke(new BasicStroke(2.0f));
-       for(int i=0; i<=1000; i+=100)
-            gc.drawLine(i,0,i,1000);
-       for(int j=0; j<=1000; j+=100)
-            gc.drawLine(0,j,1000,j);
-       //TERMINA DE DIBUJAR EL PLANO
-            
-       //dibujar eje X y Y
-       g2d.setStroke(new BasicStroke(5.0f));
-       g2d.setColor(Color.BLUE);
-       g2d.drawLine(0,500,1000,500);
-       g2d.drawLine(500,0,500,1000);
-       //termina de dibujar
-       
-       //INDICA EJES
-       Font fuente = new Font("Arial", Font.BOLD,50);
-       g2d.setFont(fuente);
-       g2d.setColor(Color.RED);
-       g2d.drawString("Y", 510, 70);
-       g2d.drawString("X", 950, 550);
-       
-       g2d.translate(500,500);//pone el punto en el medio
-       
-       
-       //Funciones
-       g2d.setColor(Color.MAGENTA);
-      
-       g2d.setStroke(new BasicStroke(5.0f));
-       GeneralPath polilinea = new GeneralPath(GeneralPath.WIND_EVEN_ODD,
-                                               puntos.size());
-                           
-       polilinea.moveTo(100*((double)puntos.elementAt(0).getX()), 100*(-1*((double)puntos.elementAt(0).getY())));
-       
-       for(int Z=1;Z< puntos.size();Z++)
-           polilinea.lineTo(100*((double)puntos.elementAt(Z).getX()), 100*(-1*((double)puntos.elementAt(Z).getY())));
-      // polilinea.closePath();   
-      
-       g2d.draw(polilinea);
-       
-       g2d.setStroke(new BasicStroke(1.0f));
-       g2d.setColor(Color.ORANGE);
-       g2d.drawString(poli.toString(), -490,-400);
-       
-       
-    }                                                                                                   
     
+    public Punto getPunto(int numero)
+    {
+        return puntos.elementAt(numero);
+    }
+    
+    public void paint(Graphics g)
+    {
+        Graphics2D G2D = (Graphics2D) g;
+        
+        //Hago el centro de todo el plano 
+        G2D.translate(450,450);
+        final float dash[]={ 15.0f };
+        
+        //Dibujo los ejes X y Y
+        G2D.setStroke(new BasicStroke(3.0f));   
+        G2D.draw( new Line2D.Float(0,420,0,-420));
+        G2D.draw( new Line2D.Float(420,0,-420,0));
+        
+        //Se dibujan todas las lineas tanto en X como en Y para cuadricular
+        for (int index = -420; index<420; index=index+22)
+        {            
+            G2D.setStroke(new BasicStroke(0.5f));
+            G2D.draw( new Line2D.Float(420, index, -420, index) );
+        }
+        for (int index = -420; index<420; index=index+22)
+        {
+            G2D.setStroke(new BasicStroke(0.5f));
+            G2D.draw( new Line2D.Float(index, 420, index, -420) );
+        }
+        Font etiqueta = new Font ("Cambria", Font.BOLD,30);
+        G2D.setFont(etiqueta);
+        G2D.setColor(Color.BLACK);
+        G2D.drawString("Y", 10,-390);
+        G2D.drawString("X", 390,-15);
+        //Dibujo la funcion
+                 
+        //Hacemos la generacion de las lineas
+        GeneralPath lineas = new GeneralPath(GeneralPath.WIND_EVEN_ODD,puntos.size());                                        
+                       
+        lineas.moveTo(50*((double)puntos.elementAt(0).getX()), 50*(-1*((double)puntos.elementAt(0).getY())));
+        //Va trazando cada parte de las lineas
+        for(int index = 1; index < puntos.size(); index++)
+        {   
+            G2D.setColor(coloritos);  
+            G2D.setStroke(new BasicStroke(3.5f));            
+            lineas.lineTo(50*((double)puntos.elementAt(index).getX()), 50*(-1*((double)puntos.elementAt(index).getY())));       
+            G2D.draw(lineas);
+            try
+            {
+                thread.sleep(100);
+            }catch( InterruptedException e )
+            {
+                e.printStackTrace();
+            }
+            G2D.setColor(coloritos);
+            setVisible(true);
+        }        
+        if (cuenta < 1 )
+        {
+            getToolkit().beep();
+            JOptionPane.showMessageDialog( null, "Graficacion Exitosa!!" );
+            cuenta++;
+        }
+    }
     public static void main()
     {
-        Polinomio polinom;
-        polinom = new Polinomio(1);
-        polinom.agregaTermino(new Termino(3, 2));
-       /* polinom.agregaTermino(new Termino(1, 1));
-        polinom.agregaTermino(new Termino(1, 3));*/
-        PuntosEcuacion vent = new PuntosEcuacion(polinom, -100,100,.1);
-        vent.resize(1000,1000);
-        vent.show();
+       /*Polinomio a = new Polinomio(1);
+       Termino b = new Termino(1,2);
+       a.agregaTermino(b);
+       PuntosEcuacion puntos_Ecuacion = new PuntosEcuacion(a,-100,100, .005);  
+       puntos_Ecuacion.setSize(900,900);
+       puntos_Ecuacion.show();*/
     }
 }
